a09cd9d863f75f7de2473d61c9709975850e7d14
kanazux <alvolivre@live.com>
2018-03-13 20:42:35 -0300

    test convert ip

--- tests/test_dec2bin.py
+++ tests/test_dec2bin.py
@@ -3,12 +3,17 @@
 
 from unittest import TestCase, main
 from collections import defaultdict
-from ipy_converter.dec2bin import dict_values
+from ipy_converter.dec2bin import dict_values, convert_ip
 
 
 class testDictValues(TestCase):
     def test_dict_values(self):
         self.assertIsInstance(dict_values(), defaultdict)
+
+
+class testConvertIp(TestCase):
+    def test_convert_ip(self):
+        self.assertRegex(convert_ip('192.168.1.1'), r"([0|1]{8}\.){3}[0|1]{8}")
 
 
 if __name__ == "__main__":
